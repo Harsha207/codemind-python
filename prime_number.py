08cee4f05afaa79654126f2b920f94cac3ab6aeb
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-26 10:22:57 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False     return True n=int(input()) if prime(n):     print('prime') else:     print('not a prime')

---
+++ prime_number.py
@@ -0,0 +1,13 @@
+def prime(n):
+    if n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+    return True
+n=int(input())
+if prime(n):
+    print('prime')
+else:
+    print('not a prime')

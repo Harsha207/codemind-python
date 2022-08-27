059081dbb6daa32b85ed90e2f7750464117bd55e
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-27 14:48:47 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False     return True def rev(n):     s=0     while(n):         r=n%10         n//=10         s=s*10+r     return s n=int(input()) if prime(n) and prime(rev(n)):     print('circular prime') elif prime(n):     print('prime but not a circular prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,21 +1,21 @@
 def prime(n):
     if n==1:
-        return 0
+        return False
     else:
         for i in range(2,int(n**0.5)+1):
             if n%i==0:
-                return 0
-    return 1
+                return False
+    return True
 def rev(n):
     s=0
     while(n):
         r=n%10
-        n=n//10
+        n//=10
         s=s*10+r
     return s
 n=int(input())
 if prime(n) and prime(rev(n)):
-    print("circular prime")
+    print('circular prime')
 elif prime(n):
     print('prime but not a circular prime')
 else:

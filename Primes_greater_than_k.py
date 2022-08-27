fbfa9a6856e3906bedc8d44c1f21b4d0d0e89c5f
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-27 15:19:30 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False     return True n=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if prime(i):         if i>=k:             c+=1 print(c)

--- Primes_greater_than_k.py
+++ Primes_greater_than_k.py
@@ -1,11 +1,11 @@
 def prime(n):
     if n==1:
-        return 0
-    else: 
+        return False
+    else:
         for i in range(2,int(n**0.5)+1):
             if n%i==0:
-                return 0
-    return 1
+                return False
+    return True
 n=int(input())
 l=list(map(int,input().split()))
 k=int(input())

d3c02f36339051d3e8cb6633a14e2161b8a83412
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-29 12:13:25 +0530

    def digits(n):     if n==0:         return 1     c=0     while(n):         n//=10         c+=1     return c n=int(input()) l=list(map(int,input().split())) a=[] for i in range(n):     a.append(digits(l[i])) x=min(a) print(a.count(x))

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,15 @@
+def digits(n):
+    if n==0:
+        return 1
+    c=0
+    while(n):
+        n//=10
+        c+=1
+    return c
+n=int(input())
+l=list(map(int,input().split()))
+a=[]
+for i in range(n):
+    a.append(digits(l[i]))
+x=min(a)
+print(a.count(x))

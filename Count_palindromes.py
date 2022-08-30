de6b264a7d0c141cda89a3ce03eb79c2dfb4730b
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-30 10:47:35 +0530

    def rev(n):     rev=0     temp=n     while n>0:         r=n%10         rev=rev*10+r         n//=10     if rev==temp:         return True     else:         return False n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if rev(i):         c+=1 print(c)

---
+++ Count_palindromes.py
@@ -0,0 +1,18 @@
+def rev(n):
+    rev=0
+    temp=n
+    while n>0:
+        r=n%10
+        rev=rev*10+r
+        n//=10
+    if rev==temp:
+        return True
+    else:
+        return False
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in l:
+    if rev(i):
+        c+=1
+print(c)

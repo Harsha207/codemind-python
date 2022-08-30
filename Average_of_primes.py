e0299aaeea156f1babdbbff47c339f544bad0350
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-30 10:19:55 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False     return True n=int(input()) l=list(map(int,input().split())) s=[] for i in l:     if prime(i):         s.append(i) d=sum(s) e=len(s) a=d/e print("%.2f"%a)

---
+++ Average_of_primes.py
@@ -0,0 +1,18 @@
+def prime(n):
+    if n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+    return True
+n=int(input())
+l=list(map(int,input().split()))
+s=[]
+for i in l:
+    if prime(i):
+        s.append(i)
+d=sum(s)
+e=len(s)
+a=d/e
+print("%.2f"%a)

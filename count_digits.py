cf2b9fd2bb081b3f8cc4dc4bac7caa10df77e136
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-30 11:22:28 +0530

    def dig(n):     if n==0:         return 1     elif n<0:         n=n*-1     c=0     while n:         n//=10         c+=1     return c n=int(input()) l=list(map(int,input().split())) s=[] for i in range(n):     s.append(dig(l[i])) print(*s)

--- count_digits.py
+++ count_digits.py
@@ -1,15 +1,16 @@
-def digit(n):
+def dig(n):
     if n==0:
         return 1
-    if n<0:
+    elif n<0:
         n=n*-1
     c=0
     while n:
-        i=n%10
-        n=n//10
+        n//=10
         c+=1
     return c
 n=int(input())
-a=list(map(int,input().split()))
+l=list(map(int,input().split()))
+s=[]
 for i in range(n):
-    print(digit(a[i]),end=' ')
+    s.append(dig(l[i]))
+print(*s)
